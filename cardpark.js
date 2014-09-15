22facb7d12faed9bd9ddd4d46d832b3c2c3e3979
erictsaiweb <erictsaiweb@gmail.com>
2014-09-16 01:36:26 +0800

    finish add class
    only autosave haven't finished.

--- cardpark.js
+++ cardpark.js
@@ -1,102 +1,154 @@
-function checklist(order,ifPark){
-    this.order = order;
-    this.ifPark = ifPark;
-}
-// check function Setting
 
-var check1 = new checklist(1,"Free");
-var check2 = new checklist(2,"Free");
-var check3 = new checklist(3,"Free");
-var check4 = new checklist(4,"Free");
-var check5 = new checklist(5,"Free");
 
-// get HTML objective
+function $(id) {return document.getElementById(id);}
+
 window.onload = function(){
-    document.getElementById("1st").innerHTML = check1.ifPark;
-    document.getElementById("2nd").innerHTML = check2.ifPark;
-    document.getElementById("3rd").innerHTML = check3.ifPark;
-    document.getElementById("4th").innerHTML = check4.ifPark;
-    document.getElementById("5th").innerHTML = check5.ifPark;
-}
 
-// parked function
+// load record
+    var getArray = localStorage.getItem("saveArray");
+    var parkArray = JSON.parse(getArray);
 
-function parked1(){
-	if (check1.ifPark == "Free"){
+if (parkArray == null) {
+    var check1 = "Free";
+    var check2 = "Free";
+    var check3 = "Free";
+    var check4 = "Free";
+    var check5 = "Free";
+    var parkArray = [check1,check2,check3,check4,check5];
+    alert("null")
+// Show original result
+   
+
+ } else {
+    
+    var check1 = parkArray[0];
+    var check2 = parkArray[1];
+    var check3 = parkArray[2];
+    var check4 = parkArray[3];
+    var check5 = parkArray[4];
+ } 
+
+
+// 整理頁面文字 
+
     var dt = new Date();
-    check1.ifPark = dt.getMonth()+1+"/"+dt.getDate()+"  "+dt.getHours()+":"+dt.getMinutes();
-    alert(check1.ifPark)
-    //javascript change class
-    }
-    else {check1.ifPark = "Free"};
-}
+    $("thismonth").innerHTML = dt.getMonth()+1+"月份使用";
+    $("1st").innerHTML = check1;
+    $("2nd").innerHTML = check2;
+    $("3rd").innerHTML = check3;
+    $("4th").innerHTML = check4;
+    $("5th").innerHTML = check5;
 
-function parked2(){
-    var dt = new Date();
-check2.ifPark = dt.getMonth()+1+"/"+dt.getDate()+"  "+dt.getHours()+":"+dt.getMinutes();
-}
-function parked3(){
-    var dt = new Date();
-check3.ifPark = dt.getMonth()+1+"/"+dt.getDate()+"  "+dt.getHours()+":"+dt.getMinutes();
-}
-function parked4(){
-    var dt = new Date();
-check4.ifPark = dt.getMonth()+1+"/"+dt.getDate()+"  "+dt.getHours()+":"+dt.getMinutes();
-}
-function parked5(){
-    var dt = new Date();
-check5.ifPark = dt.getMonth()+1+"/"+dt.getDate()+"  "+dt.getHours()+":"+dt.getMinutes();
-}
+// 整理頁面CSS
+    
+    if (check1 !== "Free") {
+        document.getElementById("1st").className = "parked";
+    };
+    if (check2 !== "Free") {
+        document.getElementById("2nd").className = "parked";
+    };
+    if (check3 !== "Free") {
+        document.getElementById("3rd").className = "parked";
+    };   
+    if (check4 !== "Free") {
+        document.getElementById("4th").className = "parked";
+    };
+    if (check5 !== "Free") {
+        document.getElementById("5th").className = "parked";
+    };
+// active click action
+
+    $("1st").onclick = function(){ 
+        if (check1== "Free"){ 
+        displayDate("1st");
+        changeClass("1st");
+        check1 = x; 
+        }
+        else {
+        reverseClass("1st");
+        check1= "Free";
+        $("1st").innerHTML = check1;
+        };
+    };
+
+    $("2nd").onclick = function(){ 
+        if (check2== "Free"){ 
+        displayDate("2nd");
+        changeClass("2nd");
+        check2 = x; 
+        }
+        else {
+        reverseClass("2nd");
+        check2= "Free";
+        $("2nd").innerHTML = check2;
+        };
+    };
+    
+    $("3rd").onclick = function(){ 
+        if (check3== "Free"){ 
+        displayDate("3rd");
+        changeClass("3rd");
+        check3= x;
+        }
+        else {
+        reverseClass("3rd");
+        check3= "Free";
+        $("3rd").innerHTML = check3;
+        };
+    };
+
+    $("4th").onclick = function(){ 
+        if (check4== "Free"){ 
+        displayDate("4th");
+        changeClass("4th");
+        check4= x ;
+        }
+        else {
+        reverseClass("4th");
+        check4 = "Free";
+        $("4th").innerHTML = check4;
+        };
+    };
+
+    $("5th").onclick = function(){ 
+        if (check5 == "Free"){ 
+        displayDate("5th");
+        changeClass("5th");
+        check5= x ;
+        }
+        else {
+        reverseClass("5th");
+        check5= "Free";
+        $("5th").innerHTML = check5;
+        };
+    };
+    
+    
+    $("check").onclick = function(){
+            parkArray = [check1,check2,check3,check4,check5];
+            localStorage.removeItem ("saveArray");     
+            localStorage.setItem("saveArray",JSON.stringify(parkArray));
+                      
+    };
+
+  };
+    
+
+// function List
+
+function displayDate(idName) {
+        var dt = new Date();
+        x= dt.getMonth()+1+"/"+dt.getDate()+"  "+dt.getHours()+":"+dt.getMinutes();
+        document.getElementById(idName).innerHTML = x;
+        }  
+
+function changeClass(idName) {
+        document.getElementById(idName).className = "parked";
+        }
+
+function reverseClass(idName) {
+        document.getElementById(idName).className = "div";
+        }
 
 
-// change testing, add date
-//var dt = new Date();
-//check2.ifPark = dt.getMonth()+1+"/"+dt.getDate()+"  "+dt.getHours()+":"+dt.getMinutes();
 
-/*
-
-
-// get HTML objective
-
-var main1 = document.getElementById("1st");
-var main2 = document.getElementById("2nd");
-var main3 = document.getElementById("3rd");
-var main4 = document.getElementById("4th");
-var main5 = document.getElementById("5th");
-
-function myFunction() { 
-    document.getElementById("5th").innerHTML = "Hello JavaScript!";
-}
-
-// load HTML
-
-
-// change check function, 0 is haven't used.
-*/
-
-
-/*
-
-if (check[1].ifPark = 0) {
-     check[1].ifPark = 1;  
-};
-else {
-	check[1].ifPark =0
-};
-return
-console.log(check[1]);
-
-//for (i=1, i<6, i=i+1) {
-	
-//}
-
-
-$(document).ready(function(){
-    $('#1st,#2nd,#3rd,#4th,#5th').click(function(){
-      $(this).toggleClass("checked");
-      $(this).text(Date())
-    });
-
-});
-
-*/
